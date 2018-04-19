4e0a17aa6b1381e0441a3a36577d3ca5f0cd6cd6
Spyros Argalias <sargalias@gmail.com>
2018-04-19 11:14:49 +0100

    basic project setup

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('home route');
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log('Server started on port ' + PORT);
+});
