d6b2a9e4b3cbbff1c3c0b04500009b6c1feef62b
Spyros Argalias <sargalias@gmail.com>
2018-04-20 10:49:28 +0100

    finish client side chart functionality

--- app.js
+++ app.js
@@ -12,8 +12,22 @@ app.get('/test', (req, res) => {
     res.render('polls/index');
 });
 
+const testPoll = {
+    title: 'Who is your favorite superhero',
+    results: [
+        {option: 'Batman', votes: 20},
+        {option: 'Superman', votes: 10},
+        {option: 'Wonder Woman', votes: 5},
+        {option: 'The Flash', votes: 23},
+    ]
+};
+
 app.get('/test2', (req, res) => {
-    res.render('polls/show', {pollData: "hello"});
+    res.render('polls/show', {
+        data: [20, 10, 5, 23],
+        colors: ["#000", "#333", "#555", "#999"],
+        labels: ["Superman", "Batman", "Wonder Woman", "The Flash"]
+    });
 });
 
 
@@ -21,3 +35,11 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log('Server started on port ' + PORT);
 });
+
+// const PollSchema = new mongoose.Schema({
+//     title: {type: String, required: true},
+//     results: [{
+//         option: {type: String, required: true},
+//         votes: {type: Number, required: true}
+//     }]
+// });
