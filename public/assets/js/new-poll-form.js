1117233ffce890ae29038db8dedd0644545e4655
Spyros Argalias <sargalias@gmail.com>
2018-04-24 15:23:43 +0100

    poll new js, make additional options required

--- public/assets/js/new-poll-form.js
+++ public/assets/js/new-poll-form.js
@@ -12,8 +12,11 @@ function addOption() {
         placeholder: originalOption.getAttribute('placeholder'),
         name: originalOption.getAttribute('name'),
         type: originalOption.getAttribute('type'),
-        class: originalOption.getAttribute('class')
+        class: originalOption.getAttribute('class'),
     };
+    if (originalOption.hasAttribute('required')) {
+        attr.required = true;
+    }
     let newOption = document.createElement('input');
     for (let key in attr) {
         newOption.setAttribute(key, attr[key]);
