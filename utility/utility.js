5495a9e6366085e0e026df800e4d39729484fe0d
Spyros Argalias <sargalias@gmail.com>
2018-04-25 18:03:54 +0100

    create userOwnsPoll middleware which checks the user owns the poll and can perform actions on it.

--- utility/utility.js
+++ utility/utility.js
@@ -1,3 +1,7 @@
+const Poll = require('../models/poll');
+const User = require('../models/user');
+
+
 function isLoggedIn(req, res, next) {
     if (req.user) {
         return next();
@@ -20,6 +24,27 @@ function isLoggedInMessage(message) {
     }
 }
 
-module.exports.isLoggedInMessage = isLoggedInMessage;
+function userOwnsPoll(req, res, next) {
+    // Assumes req.params.poll_id exists
+    // Assumes req.user exists
+    Poll.findById(req.params.poll_id)
+        .populate('author', '_id')
+        .exec((err, poll) => {
+            if (err || !poll) {
+                err = new Error('Poll not found');
+                err.status(404);
+                return next(err);
+            }
+            if (poll.user._id.equals(req.user.id)) {
+                return next();
+            }
+            err = new Error('Not authorized');
+            err.status(500);
+            return next(err);
+        });
+}
 
+
+module.exports.isLoggedInMessage = isLoggedInMessage;
 module.exports.isLoggedIn = isLoggedIn;
+module.exports.userOwnsPoll = userOwnsPoll;
