d173024b653cf28dacf66d0d43c8aa69e7e4b447
Spyros Argalias <sargalias@gmail.com>
2018-04-25 18:25:00 +0100

    fix userOwnsPoll middleware

--- utility/utility.js
+++ utility/utility.js
@@ -27,21 +27,14 @@ function isLoggedInMessage(message) {
 function userOwnsPoll(req, res, next) {
     // Assumes req.params.poll_id exists
     // Assumes req.user exists
-    Poll.findById(req.params.poll_id)
-        .populate('author', '_id')
-        .exec((err, poll) => {
-            if (err || !poll) {
-                err = new Error('Poll not found');
-                err.status(404);
-                return next(err);
-            }
-            if (poll.user._id.equals(req.user.id)) {
-                return next();
-            }
-            err = new Error('Not authorized');
-            err.status(500);
-            return next(err);
-        });
+    for (let pollId of req.user.polls) {
+        if (pollId.equals(req.params.poll_id)) {
+            return next();
+        }
+    }
+    let err = new Error('Not authorized');
+    err.status = 500;
+    return next(err);
 }
 
 
