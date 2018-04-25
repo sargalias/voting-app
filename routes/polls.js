bd60f7169e19a5c44b26f36eb6a61c2dc09e7977
Spyros Argalias <sargalias@gmail.com>
2018-04-25 14:48:25 +0100

    route for edit poll

--- routes/polls.js
+++ routes/polls.js
@@ -11,6 +11,8 @@ router.post('/', ut.isLoggedInMessage("Not authorized"), pc.newPollValidation, p
 
 router.get('/:id', pc.show);
 
+router.get('/:id/edit', pc.edit);
+
 router.delete('/:id',
     ut.isLoggedInMessage('Not authorized'),
     pc.delete);
