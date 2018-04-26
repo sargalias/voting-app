f095064c62606a71b5c922dca3711e8c25e6f942
Spyros Argalias <sargalias@gmail.com>
2018-04-26 17:39:31 +0100

    replace relative URL with environment variable containing absolute URL for google callbackURL

--- config/auth-google.js
+++ config/auth-google.js
@@ -5,7 +5,7 @@ module.exports = function(passport) {
     passport.use(new GoogleStrategy({
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "/auth/google/callback"
+            callbackURL: process.env.DOMAIN_URI + "/auth/google/callback"
         },
         function(accessToken, refreshToken, profile, done) {
             User.findOne({googleId: profile.id}, (err, user) => {
