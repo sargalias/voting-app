32ee80cc17d9ede067b689b0532c42b93a7e8df4
Spyros Argalias <sargalias@gmail.com>
2018-04-26 17:30:49 +0100

    change database config to production instead of initial setup

--- config/database.js
+++ config/database.js
@@ -1,7 +1,6 @@
 // Mongoose and MongoDB
 const mongoose = require('mongoose');
-// mongoose.connect(process.env.MONGODB_URI);
-mongoose.connect('mongodb://localhost/fcc_voting_app');
+mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
